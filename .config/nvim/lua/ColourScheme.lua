52ef88d670d3e8c8638d0ec3cdad565f3ce99304
Tim Roper <tim.roper@ovoenergy.com.au>
2021-08-03 16:10:28 +1000

    Colour scheme

--- .config/nvim/lua/ColourScheme.lua
+++ .config/nvim/lua/ColourScheme.lua
@@ -1,1 +1,4 @@
+-- Config related to colour schemes etc.
+-- Below references a symlink under path ~/.config/nvim/colors/xx
+-- Where the link points to a git submodule that is a colorscheme 
 vim.cmd('colorscheme PaperColorSlim')
