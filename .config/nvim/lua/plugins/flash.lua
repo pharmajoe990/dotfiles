cb48da65e1297b44f05896f2f64e96107558454f
Tim Roper <tim.roper@ovoenergy.com.au>
2024-05-31 08:44:20 +1000

    LazyVim further config

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,4 @@
+return {
+  --disable search for now until can configure it correctly
+  { "folke/flash.nvim", enabled = false },
+}
--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,9 @@ return {
       ensure_installed = {
         "hcl",
         "terraform",
+        "lua",
+        "jsonc",
+        "typescript",
       },
     },
   },
